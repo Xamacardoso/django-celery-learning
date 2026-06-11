07e42fd79f577444e20efddd746afdb356e1f695
Xamacardoso <contato.xcardoso@gmail.com>
2026-06-11 11:36:07 -0300

    :sparkles: Feat (Docker): added docker compose, dockerfile and entrypoint settings. App now runs normally

--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -9,23 +9,14 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&xa&btt^!2f(t74!*64mv+*zz+mlyip1qm_+p&7s)_50nasl10'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
+DEBUG = os.environ.get('DEBUG', False)
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
