7b87f789a52fbeebd680cd9f8219a403221348c2
Xamacardoso <contato.xcardoso@gmail.com>
2026-06-18 11:42:52 -0300

    :sparkles: Feat (Task Routing): Added celery task routing configuration with different queues for each celery instance

--- celeryworker/celerytask.py
+++ celeryworker/celerytask.py
@@ -10,6 +10,8 @@ app = Celery('task')
 # Configura o CELERY usando settings do arquivo celeryconfig.py
 app.config_from_object('celeryconfig', namespace='CELERY')
 
-@app.task
-def add_numbers(x, y):
-    return x + y
+# importa as tasks definidas em newapp.tasks
+app.conf.imports = ('newapp.tasks')
+
+# Diz pro celery para buscar tasks
+app.autodiscover_tasks()
--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -14,11 +14,12 @@ app = Celery('dcelery')
 # Configura o CELERY usando settings do django
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Task para somar numeros
-@app.task
-def add_numbers(x, y):
-    return x + y
+# Configura as rotas de tasks para different queues (diferentes filas)
+# Isso diz pro celery que a task shared_test deve ser processada pela queue1 e shared_test2 pela queue2
+app.conf.task_routes = {
+    'newapp.tasks.shared_test': {'queue': 'queue1'},
+    'newapp.tasks.shared_test2': {'queue': 'queue2'},
+}
 
-# Diz pro celery para buscar tasks nas apps definidas em
-# INSTALLED_APPS no settings.py
-app.autodiscover_tasks(['newapp'])
+# Diz pro celery para buscar tasks
+app.autodiscover_tasks()
